ac44b4deb8f200fb41ca1c839aec36057c3e386d
chl.cken <ckdgus988@naver.com>
2023-12-17 21:24:56 +0900

    feature: django Mysql 연결 및 sueper계정 생성

--- plabtiercheck/settings.py
+++ plabtiercheck/settings.py
@@ -94,8 +94,7 @@ WSGI_APPLICATION = 'plabtiercheck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'plabtiercheckDB',
         'USER': 'root',
         'PASSWORD': '1212',
