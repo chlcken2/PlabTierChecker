5f1da9eb87c2c826db82072258bc10d630ca8e8d
chl.cken <ckdgus988@naver.com>
2023-12-18 18:37:48 +0900

    feature: 웹 header이미지 및 레이아웃 추가

--- plabtiercheck/settings.py
+++ plabtiercheck/settings.py
@@ -138,7 +138,12 @@ TIME_ZONE = 'Asia/Seoul'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'plabtiercheck/static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
