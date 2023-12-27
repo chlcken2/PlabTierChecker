9315003689535f7f7a8d7394e8c13da3c39b24ee
chl.cken <ckdgus988@naver.com>
2023-12-27 17:13:47 +0900

    Feature: 테스트 user 및 player, player_info생성

--- plabtiercheck_app/management/commands/create_users.py
+++ plabtiercheck_app/management/commands/create_users.py
@@ -9,10 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         fake = Faker()
 
-        # 30명의 유저 생성
         for _ in range(30):
+            # 유저 생성
             username = fake.user_name()
             password = 'password'
-            User.objects.create_user(username=username, password=password)
+            user = User.objects.create_user(username=username, password=password)
 
-        self.stdout.write(self.style.SUCCESS('Successfully created 30 users'))
+            # 자동 생성된 Player 및 Player_info 인스턴스 검색
+            player = user.player
+            player_info = player.player_info
+
+            # Faker로부터 무작위 데이터로 Player_info 업데이트
+            player_info.point = fake.random_int(min=0, max=1000)  # 0과 1000 사이의 무작위 점수
+            player_info.is_celebrity = fake.boolean()  # 무작위 부울 값
+            player_info.player_tier = fake.random_int(min=0, max=16)
+            player_info.game_participate_count = fake.random_int(min=0, max=1000)
+
+            # 업데이트된 Player_info 인스턴스 저장
+            player_info.save()
+
+        self.stdout.write(self.style.SUCCESS('30명의 유저와 player 정보가 성공적으로 생성되었습니다'))
