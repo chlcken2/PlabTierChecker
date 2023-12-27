03a9ae6b25bca9411bb03efeb98a32293d8d27fe
chl.cken <ckdgus988@naver.com>
2023-12-27 17:12:11 +0900

    Feature: tier_type convertor 추가

--- plabtiercheck_app/utils/convertor.py
+++ plabtiercheck_app/utils/convertor.py
@@ -1,6 +1,11 @@
-from plabtiercheck_app.models import StandardDataSource
+from plabtiercheck_app.models import StandardDataSource, Player_info
 
 
 def get_game_type_display(game_type_code):
     game_types = dict(StandardDataSource.GAME_TYPES)
     return game_types.get(game_type_code, "Unknown")
+
+#
+def get_tier_display(tier_code):
+    player_tier = dict(Player_info.PLAYER_TIER_TYPE)
+    return player_tier.get(tier_code, "Unknown")
