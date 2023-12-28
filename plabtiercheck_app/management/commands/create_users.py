bcf5775dfdcb946df696f8dd9d17b0c85314f01a
chl.cken <ckdgus988@naver.com>
2023-12-28 15:39:21 +0900

    Refactor: fake player생성시 path, all_play_time 정보 추가

--- plabtiercheck_app/management/commands/create_users.py
+++ plabtiercheck_app/management/commands/create_users.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from faker import Faker
+from plabtiercheck_app.models import Player
 
 
 class Command(BaseCommand):
@@ -19,13 +20,15 @@ class Command(BaseCommand):
             player = user.player
             player_info = player.player_info
 
+            Player.player_path = fake.slug()  # 무작위 슬러그
             # Faker로부터 무작위 데이터로 Player_info 업데이트
             player_info.point = fake.random_int(min=0, max=1000)  # 0과 1000 사이의 무작위 점수
             player_info.is_celebrity = fake.boolean()  # 무작위 부울 값
             player_info.player_tier = fake.random_int(min=0, max=16)
             player_info.game_participate_count = fake.random_int(min=0, max=1000)
-
+            player_info.all_play_time = fake.random_int(min=0, max=1000)
             # 업데이트된 Player_info 인스턴스 저장
+            Player.save()
             player_info.save()
 
         self.stdout.write(self.style.SUCCESS('30명의 유저와 player 정보가 성공적으로 생성되었습니다'))
